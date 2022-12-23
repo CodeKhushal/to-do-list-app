a6540af3a0390c33cbcace92844980d0f2847fd0
CodeKhushal <khushalsachdeva22@gmail.com>
2022-12-24 01:38:11 +0530

    First Commit for to-do-app by Khushal

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <a className="logo" href="/"></a>
+    </nav>
+  )
+}
+
+export default Navbar
